b55e4086e937388904917b11d65b02531472c7e4
pavel <ufamail.com2@gmail.com>
2023-03-09 21:58:11 +0500

    start deploy

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express();
 const ProductsController = require("./controllers/products");
 const UsersController = require("./controllers/users")
 
-app.listen("cheese-back-production.up.railway.app");
+const serverless = require('serverless-http');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.get('/api/info', (req, res) => {
+    res.send({ application: 'sample-app', version: '1.0' });
+});
+app.post('/api/v1/getback', (req, res) => {
+    res.send({ ...req.body });
+});
 
 app.use("/prod", ProductsController);
 app.use("/users", UsersController);
@@ -12,3 +20,5 @@ app.get("/", (req, res) => {
   res.status(200);
   res.send("")
 })
+
+module.exports.handler = serverless(app);
