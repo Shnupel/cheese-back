de0ba3cf7557efd1f9bd8be0960d8bf46d114301
pavel <ufamail.com2@gmail.com>
2023-03-27 17:02:48 +0500

    change bugs

--- models/users/index.js
+++ models/users/index.js
@@ -58,13 +58,42 @@ class UsersModel {
         if(users[userID].products.includes(product)){
           return reject({ success: false, message: "this is includes" });
         }
-        users[userID].products.push(product);
+        const productId = users[userID].products.findIndex(({ id }) => product.id == id);
+        if(productId == -1){
+          users[userID].products.push(product);
+        }else{
+          console.log(users[userID].products, productId);
+          users[userID].products[productId].counter++;
+        }
         fs.writeFile(DBPath, JSON.stringify({ users }), (err) => {
           if(err) return reject({ success: false, err: error });
           resolve({ success: true });
         })
       })
     });
+  }
+
+  static removeProduct({ email: userEmail, product }){
+    return new Promise((resolve, reject) => {
+      fs.readFile(DBPath, "utf-8", (err, result) => {
+        if(err) return reject(err);
+        const { users } = JSON.parse(result);
+        const userID = users.findIndex(({ email }) => email === userEmail);
+        if(users[userID].products.includes(product)){
+          return reject({ success: false, message: "this is includes" });
+        }
+        const productId = users[userID].products.findIndex(({ id }) => product.id == id);
+        if(users[userID].products[productId].counter == 1){
+          users[userID].products = users[userID].products.filter(({ id }) => id != product.id);
+        }else{
+          users[userID].products[productId].counter--
+        }
+        fs.writeFile(DBPath, JSON.stringify({ users }), (err) => {
+          if(err) return reject({ success: false, err: error });
+          resolve({ success: true });
+        })
+      })
+    })
   }
 }
 
